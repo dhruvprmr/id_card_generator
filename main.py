b9a5046e8d89e0985c200a251de1200f6a4edc54
Dhruv Parmar <dhruvparmar1112@gmail.com>
2025-06-11 09:45:47 -0500

    Initial commit - ID card generator

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+from PIL import Image, ImageDraw, ImageFont
+from fpdf import FPDF
+
+TEMPLATE="ute_id_template.png"
+CSV_FILE="employee.csv"
+PROFILE_PICTURE="profile_images"
+OUTPUT_PDF="employee_id.pdf"
+
+ID_WIDTH, ID_HEIGHT = 325, 204
+PHOTO_SIZE = (110, 110)
+PHOTO_POSITION = (180, 25)
+NAME_POSITION = (20, 170)
+FONT_SIZE = 14
+FONT_PATH = "/System/Library/Fonts/Supplemental/Arial Bold.ttf"
+TEXT_COLOR = "white"
+
+#Reading the data from csv
+employees = pd.read_csv(CSV_FILE).to_dict(orient='records')
+
+#Initializing PDF creation
+pdf = FPDF(unit='pt', format=[ID_WIDTH, ID_HEIGHT])
+
+for i in range(len(employees)):
+    name = employees[i]['name']
+    title = employees[i]['title']
+    profile_path = os.path.join(PROFILE_PICTURE, employees[i]['profile'])
+
+    id_template = Image.open(TEMPLATE)
+
+    try:
+        photo = Image.open(profile_path).resize(PHOTO_SIZE)
+        id_template.paste(photo, PHOTO_POSITION)
+    except FileNotFoundError:
+        continue
+
+    #Drawing the data on template
+    draw = ImageDraw.Draw(id_template)
+    font = ImageFont.truetype(FONT_PATH,FONT_SIZE)
+    draw.text(NAME_POSITION, name, fill=TEXT_COLOR, font=font)
+
+    temp_image_path = f"temp_{i}.png"
+    id_template.save(temp_image_path)
+
+    pdf.add_page()
+    pdf.image(temp_image_path, x=0, y=0, w=ID_WIDTH, h=ID_HEIGHT)
+
+pdf.output(OUTPUT_PDF)
